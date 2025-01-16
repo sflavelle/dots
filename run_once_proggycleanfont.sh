3cf28c597855f73e05444cae3922cf40d3bc7b65
Lily Flavelle <me@neurario.com>
2025-01-16 22:52:07 +1100

    fix font thing

--- run_once_proggycleanfont.sh
+++ run_once_proggycleanfont.sh
@@ -4,7 +4,8 @@ command -v curl >/dev/null 2>&1 || { echo >&2 "Installing ProggyClean Nerd Fonts
 
 set -ex
 
-curl -o ~/.fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/ProggyClean.zip
+mkdir ~/.fonts
+curl -o ~/.fonts/ProggyCNerdFont.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/ProggyClean.zip
 mkdir ~/.fonts/ProggyClean
-unzip ProggyClean.zip -d ~/.fonts/ProggyClean
+unzip ProggyCNerdFont.zip -d ~/.fonts/ProggyClean
 echo "Installed ProggyClean nerd fonts!"
